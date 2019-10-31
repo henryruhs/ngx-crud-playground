86dc4bf1ee44deb1bc184b8299421c83f4287e71
redaxmedia <info@redaxmedia.com>
2019-10-31 12:45:34 +0100

    Try to fix Travis and Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const path = require('path');
 const express = require('express');
 const server = express();
+const path = require('path');
 
 server.use(express.static(path.resolve('./dist/ngx-crud-playground')));
 server.get('/*', (request, response) =>
