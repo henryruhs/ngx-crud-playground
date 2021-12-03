c69d646cfc1e20cfe4abb3185289366e0e19cda9
redaxmedia <info@redaxmedia.com>
2021-12-03 14:22:00 +0100

    Update to ngx-crud@10.0.0-next.3

--- src/app/joke/joke.service.ts
+++ src/app/joke/joke.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CrudService } from 'ngx-crud';
+import { ApiUrl, ApiRoute, CrudService } from 'ngx-crud';
 import { RequestBody, ResponseBody } from './joke.interface';
 
 import { environment } from '../../environments/environment';
 
 @Injectable()
+@ApiUrl(environment.apiUrl)
+@ApiRoute(environment.apiRoutes.joke)
 export class JokeService extends CrudService<RequestBody, ResponseBody>
 {
-	protected apiUrl : string = environment.apiUrl;
-	protected apiRoute : string = environment.apiRoutes.joke;
 }
