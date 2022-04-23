ec10ce4b2d4d44525d0cfce50540883202874032
henryruhs <info@henryruhs.com>
2022-04-23 10:01:11 +0200

    Move testing to Jest part2

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,10 +1,4 @@
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(),
-{
-	teardown:
-	{
-		destroyAfterEach: true
-	}
-});
+getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
