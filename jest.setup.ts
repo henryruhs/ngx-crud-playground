992283c1a1fca5cd5f2562fcbfb64d289ebbcfbc
henryruhs <info@henryruhs.com>
2022-04-23 10:52:44 +0200

    Move testing to Jest part5

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,1 @@
-import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+import 'jest-preset-angular/setup-jest';
