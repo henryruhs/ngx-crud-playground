ba68e37cc17b89576ca0e061628de7f0150bb1d5
henryruhs <info@henryruhs.com>
2022-06-06 23:25:53 +0200

    Cleanup jest testing a bit

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,8 @@
-module.exports =
+export default
 {
 	preset: 'jest-preset-angular',
 	setupFilesAfterEnv:
 	[
 		'<rootDir>/jest.setup.ts'
-	],
-	globalSetup: 'jest-preset-angular/global-setup'
+	]
 };
