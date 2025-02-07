82c8173da08e55b01971a4359e8e88f6653d579f
henryruhs <info@henryruhs.com>
2025-02-07 15:08:47 +0100

    Modernize codebase

---
+++ eslint.config.cjs
@@ -0,0 +1,7 @@
+module.exports =
+[
+	...require('@isnotdefined/eslint-config/angular'),
+	...require('@isnotdefined/eslint-config/common'),
+	...require('@isnotdefined/eslint-config/cypress'),
+	...require('@isnotdefined/eslint-config/jest')
+]
--- src/app/app.component.e2e.ts
+++ src/app/app.component.e2e.ts
@@ -2,11 +2,11 @@ import { Environment, environmentHelper } from '@environments';
 
 const environment : Environment = environmentHelper(Cypress.env('APP_ENV'));
 
-describe('Root', () =>
+describe('root', () =>
 {
 	beforeEach(() => cy.visit(environment.baseUrl));
 
-	it('Should have elements', () =>
+	it('should have elements', () =>
 	{
 		cy.get('app-content').should('be.visible');
 		cy.get('app-panel').should('be.visible');
